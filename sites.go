6c8b44ef3e0e85ae09bc27b551163d995e6a8728
tsak <1022521+tsak@users.noreply.github.com>
2020-03-05 21:29:10 +0000

    add comments to sites

--- sites.go
+++ sites.go
@@ -1,41 +1,26 @@
 package main
 
 import (
-	"bufio"
 	"encoding/csv"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"os"
 )
 
-// readLines reads a whole file into memory
-// and returns a slice of its lines.
-func readLines(path string) ([]string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	return lines, scanner.Err()
-}
-
+// LoadSites loads sites from CSV and returns a new SiteMap
 func LoadSites(path string) *SiteMap {
 	sm := NewSiteMap()
 
-	csvfile, err := os.Open(path)
+	// Open CSV
+	file, err := os.Open(path)
 	if err != nil {
 		log.WithField("File", path).WithError(err).Fatal("Unable to load sites")
 	}
 
-	r := csv.NewReader(csvfile)
+	// Read CSV
+	r := csv.NewReader(file)
 	for {
-		// Read each fields from csv
+		// Read fields from CSV
 		fields, err := r.Read()
 
 		if err == io.EOF {
@@ -46,6 +31,7 @@ func LoadSites(path string) *SiteMap {
 			log.WithError(err).Fatal("CSV read")
 		}
 
+		// Ignore lines that don't have two entries exactly
 		if len(fields) != 2 {
 			continue
 		}
